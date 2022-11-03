8ee4dacf886ac43c8df93adf769c7c9b5ce02d34
Folka2134 <folka2134@gmail.com>
2022-11-03 14:04:30 +0000

    craeted userModel and installed bcrypt

---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    require: true,
+    type: String,
+  },
+  email: {
+    require: true,
+    type: String,
+  },
+  password: {
+    require: true,
+    type: String,
+  },
+});
+
+// hashPassword
+
+userSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Helper method for validating user's password.
+
+userSchema.methods.comparePassword = function comparePassword(
+  candidatePassword,
+  cb
+) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+module.exports = mongoose.model("Users", userSchema);
