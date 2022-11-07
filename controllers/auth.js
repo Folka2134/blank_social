c96b1a24aa7efe25d36bd16daf84b6e6b57e494a
Folka2134 <folka2134@gmail.com>
2022-11-07 09:45:29 +0000

    fixed bug with signup functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,6 +17,7 @@ module.exports = {
     }
   },
   postSignup: async (req, res) => {
+    console.log(req.body.confirmPassword);
     const validationErrors = [];
     if (!validator.isEmail(req.body.email))
       validationErrors.push({ msg: "Please enter a valid email address." });
@@ -27,10 +28,11 @@ module.exports = {
     if (req.body.password !== req.body.confirmPassword)
       validationErrors.push({ msg: "Passwords do not match" });
 
-    /*     if (validationErrors.length) {
-      req.flash("errors", validationErrors);
+    if (validationErrors.length) {
+      console.log(validationErrors);
+      // req.flash("errors", validationErrors);
       return res.redirect("../signup");
-    } */
+    }
     req.body.email = validator.normalizeEmail(req.body.email, {
       gmail_remove_dots: false,
     });
@@ -44,21 +46,25 @@ module.exports = {
     });
 
     User.findOne(
-      { $or: [{ email: req.body.email }, { username: req.body.userName }] },
+      { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
       (err, existingUser) => {
         if (err) {
           return next(err);
         }
-        // if (existingUser) {
-        //   req.flash("errors", {
-        //     msg: "Account with that email address or username already exists.",
-        //   });
-        //   return res.redirect("../signup");
-        // }
+        if (existingUser) {
+          // req.flash("errors", {
+          //   msg: "Account with that email address or username already exists.",
+          // });
+          console.log(
+            "Account with that email address or username already exists."
+          );
+          return res.redirect("../signup");
+        }
         user.save((err) => {
           if (err) {
             return next(err);
           }
+          res.redirect("/feed");
           // req.logIn(user, (err) => {
           //   if (err) {
           //     return next(err);
