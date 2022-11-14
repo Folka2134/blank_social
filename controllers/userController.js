ab259f26a5a356ac5972d7216688936f0f2f5a86
Folka2134 <folka2134@gmail.com>
2022-11-14 10:32:07 +0000

    feed now shows user posts

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,12 @@ const Post = require("../models/postModel");
 module.exports = {
   getFeedPage: async (req, res) => {
     try {
-      res.render("feed.ejs");
+      const postItems = await Post.find();
+      console.log(postItems);
+      res.render("feed.ejs", {
+        posts: postItems,
+        user: req.user,
+      });
     } catch (error) {
       console.log(error);
     }
