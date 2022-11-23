622a6ee76ec39f179a5a8414b61c89520a777896
Folka2134 <folka2134@gmail.com>
2022-11-23 13:17:39 +0000

    cleaned code

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,21 +55,4 @@ module.exports = {
       console.log(err);
     }
   },
-
-  //   app.put('/addOneLike', (request, response) => {
-  //     db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
-  //         $set: {
-  //             likes:request.body.likesS + 1
-  //           }
-  //     },{
-  //         sort: {_id: -1},
-  //         upsert: true
-  //     })
-  //     .then(result => {
-  //         console.log('Added One Like')
-  //         response.json('Like Added')
-  //     })
-  //     .catch(error => console.error(error))
-
-  // })
 };
