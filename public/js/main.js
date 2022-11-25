691db23be23d4ec86d18bbb72ea0b58535b7fca0
Folka2134 <folka2134@gmail.com>
2022-11-25 10:25:16 +0000

    cleaned code

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,7 @@ const createPostBtn = document.querySelector(".openModal");
 const closeBtn = document.querySelector(".close");
 const body = document.querySelector("main");
 const nav = document.querySelector("nav");
-const imageForm = document.querySelector("#image-form");
-// const imgInput = document.querySelector("#img_upload");
+const imgInput = document.querySelector("#img_upload");
 // const likeText = document.querySelectorAll(".like");
 
 createPostBtn.addEventListener("click", () => {
@@ -21,43 +20,9 @@ window.addEventListener("click", (event) => {
   }
 });
 
-imageForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(e);
-});
-
-// imgInput.onchange = (event) => {
-//   const [file] = imgInput.files;
-//   if (file) {
-//     img_preview.src = URL.createObjectURL(file);
-//   }
-// };
-
-// likeText.forEach((element) => {
-//   element.addEventListener("click", addLike);
-// });
-
-// async function addLike() {
-//   const createdBy = this.parentNode.parentNode.childNodes[3].textContent;
-//   const caption = this.parentNode.parentNode.childNodes[5].textContent;
-//   const likes = Number(
-//     this.parentNode.parentNode.childNodes[7].childNodes[1].textContent
-//   );
-
-//   try {
-//     const reponse = await fetch("posts/addLike", {
-//       method: "put",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         createdBy: createdBy,
-//         caption: caption,
-//         likes: likes,
-//       }),
-//     });
-//     const data = await reponse.json();
-//     console.log(data);
-//     location.reload();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+imgInput.onchange = (event) => {
+  const [file] = imgInput.files;
+  if (file) {
+    img_preview.src = URL.createObjectURL(file);
+  }
+};
