4e9ec8c1ed6156ea520ab4b15bde7af6f62ff97f
Folka2134 <folka2134@gmail.com>
2022-11-25 10:24:44 +0000

    cleaned code

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,30 @@
-// const modal = document.querySelector(".postModal");
-// const createPostBtn = document.querySelector(".openModal");
-// const closeBtn = document.querySelector(".close");
-// const body = document.querySelector("main");
-// const nav = document.querySelector("nav");
+const modal = document.querySelector(".postModal");
+const createPostBtn = document.querySelector(".openModal");
+const closeBtn = document.querySelector(".close");
+const body = document.querySelector("main");
+const nav = document.querySelector("nav");
+const imageForm = document.querySelector("#image-form");
 // const imgInput = document.querySelector("#img_upload");
 // const likeText = document.querySelectorAll(".like");
 
-// createPostBtn.addEventListener("click", () => {
-//   modal.style.display = "block";
-// });
+createPostBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
 
-// closeBtn.addEventListener("click", () => {
-//   modal.style.display = "none";
-// });
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-// window.addEventListener("click", (event) => {
-//   if (event.target == body || event.target == nav) {
-//     modal.style.display = "none";
-//   }
-// });
+window.addEventListener("click", (event) => {
+  if (event.target == body || event.target == nav) {
+    modal.style.display = "none";
+  }
+});
+
+imageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+});
 
 // imgInput.onchange = (event) => {
 //   const [file] = imgInput.files;
@@ -38,9 +44,6 @@
 //     this.parentNode.parentNode.childNodes[7].childNodes[1].textContent
 //   );
 
-//   console.log(createdBy);
-//   console.log(caption);
-//   console.log(likes);
 //   try {
 //     const reponse = await fetch("posts/addLike", {
 //       method: "put",
